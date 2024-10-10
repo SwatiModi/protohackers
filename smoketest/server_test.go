df5f26897f7c3b5da6cb49028852f9afa1709b09
Swati Modi <swatimodi1609@gmail.com>
2024-10-10 22:55:39 +0530

    add tests for smoketest

--- smoketest/server_test.go
+++ smoketest/server_test.go
@@ -1,1 +1,36 @@
 package smoketest
+
+import (
+	"bufio"
+	"net"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+)
+
+func TestSmokeServer(t *testing.T) {
+	t.Run("basic case", func(t *testing.T) {
+		// start server
+		go StartServer()
+
+		// make a request to localhost 8000
+		conn, err := net.Dial("tcp", "localhost:8000")
+		if err != nil {
+			t.Fatal("failed to start server")
+		}
+
+		defer conn.Close()
+
+		msg := "hello World!\n"
+		if _, err := conn.Write([]byte(msg)); err != nil {
+			t.FailNow()
+		}
+
+		resp, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			t.FailNow()
+		}
+
+		assert.Equal(t, msg, resp)
+	})
+}
