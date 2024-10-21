c1028694fdd2d6d016e254a0170a73ae6298d4d7
Swati Modi <swatimodi1609@gmail.com>
2024-10-21 15:30:30 +0530

    tests for decoding, and server

--- meanstoend/server.go
+++ meanstoend/server.go
@@ -1,9 +1,47 @@
 package meanstoend
 
 import (
+	"bufio"
 	"log"
 	"net"
+	"sync"
 )
+
+func DecodeInput(b []byte) (string, int32, int32) {
+	return "I", 0, 0
+}
+
+type RequestType int
+
+const (
+	Invalid RequestType = iota
+	I
+	Q
+)
+
+func ParseRequestType(s string) RequestType {
+	switch s {
+	case "I":
+		return I
+	case "Q":
+		return Q
+	default:
+		return Invalid
+	}
+}
+
+var connData = sync.Map{}
+
+type priceData struct {
+	timestamp int32
+	price     int32
+}
+
+var emptyResponse = []byte{0x00, 0x00, 0x00, 0x00}
+
+func encodeResponse(v int) []byte {
+	return emptyResponse
+}
 
 func StartServer() {
 
@@ -34,10 +72,58 @@ func StartServer() {
 }
 
 func handleRequest(conn net.Conn) {
-	log.Println("connID", conn.LocalAddr().String(), "local", conn.RemoteAddr().String())
-	conn.Write([]byte("connID" + conn.LocalAddr().String() + "local" + conn.RemoteAddr().String() + "\n"))
-}
+	// cant really close the connection here, how does this work /????
+	// wait until client closes it
+	connID := conn.LocalAddr()
 
-func hexToDecimal() {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		inBytes := scanner.Bytes()
 
+		rt, t1, t2 := DecodeInput(inBytes)
+
+		switch ParseRequestType(rt) {
+		case I:
+			{
+				pd := priceData{
+					timestamp: t1,
+					price:     t2,
+				}
+
+				// insert price data
+				if v, ok := connData.Load(connID); !ok {
+					if recs, valid := v.([]priceData); valid {
+						recs = append(recs, pd)
+						connData.Store(connID, recs)
+					}
+				}
+
+				connData.Store(connID, []priceData{pd})
+			}
+
+		case Q:
+			{
+				v, ok := connData.Load(connID)
+				if !ok {
+					conn.Write(emptyResponse)
+				}
+
+				recs := v.([]priceData)
+				sum := 0
+				numRecs := 0
+				for _, rec := range recs {
+					if rec.timestamp >= t1 && rec.timestamp >= t2 {
+						sum += int(rec.price)
+						numRecs += 1
+					}
+				}
+
+				average := sum / numRecs
+
+				conn.Write(encodeResponse(int(average)))
+			}
+		default:
+		}
+
+	}
 }
--- meanstoend/server_test.go
+++ meanstoend/server_test.go
@@ -5,18 +5,25 @@ import (
 	"net"
 	"protohackers/meanstoend"
 	"testing"
+
+	"github.com/go-playground/assert/v2"
 )
 
-func TestServer(t *testing.T) {
-	t.Run("basic case test", func(t *testing.T) {
-		go meanstoend.StartServer()
+var insertInput = []byte{0x49, 0x00, 0x00, 0x30, 0x39, 0x00, 0x00, 0x00, 0x65}
+var queryInput = []byte{0x51, 0x00, 0x00, 0x03, 0xe8, 0x00, 0x01, 0x86, 0xa0}
+var invalidInput = []byte{0x55, 0x00, 0x00, 0x30, 0x39, 0x00, 0x00, 0x00, 0x65}
+var outofTimeRangeInput = []byte{0x51, 0x00, 0x00, 0x00, 0xe8, 0x00, 0x01, 0x00, 0xa0}
 
+func TestServer(t *testing.T) {
+	go meanstoend.StartServer()
+
+	t.Run("valid insert op", func(t *testing.T) {
 		conn, err := net.Dial("tcp", "localhost:8000")
 		if err != nil {
 			t.Fail()
 		}
 
-		if _, err := conn.Write([]byte("hello")); err != nil {
+		if _, err := conn.Write(insertInput); err != nil {
 			t.Error("write", err)
 		}
 
@@ -27,5 +34,87 @@ func TestServer(t *testing.T) {
 
 		t.Log(resp)
 
+		assert.Equal(t, "", resp)
+	})
+
+	t.Run("valid query op", func(t *testing.T) {
+		conn, err := net.Dial("tcp", "localhost:8000")
+		if err != nil {
+			t.Fail()
+		}
+
+		if _, err := conn.Write(insertInput); err != nil {
+			t.Error("write", err)
+		}
+
+		resp, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			t.FailNow()
+		}
+
+		if _, err := conn.Write(insertInput); err != nil {
+			t.Error("write", err)
+		}
+
+		resp2, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			t.FailNow()
+		}
+
+		t.Log(resp)
+
+		assert.Equal(t, "", resp2)
+	})
+
+	t.Run("invalid query op", func(t *testing.T) {
+
+	})
+
+	t.Run("insert with conn a, query with conn a", func(t *testing.T) {
+
+	})
+
+	t.Run("insert with conn a, query with conn b", func(t *testing.T) {
+
+	})
+
+	t.Run("out of range query, no results", func(t *testing.T) {
+
+	})
+}
+
+func TestDecoding(t *testing.T) {
+	t.Run("decode insert request", func(t *testing.T) {
+
+		rType, timestamp, price := meanstoend.DecodeInput(insertInput)
+
+		rt := meanstoend.ParseRequestType(rType)
+		t.Log(rt, timestamp, price)
+
+		assert.Equal(t, meanstoend.RequestType(1), rt)
+		assert.Equal(t, 12345, timestamp)
+		assert.Equal(t, 101, price)
+	})
+
+	t.Run("decode query request", func(t *testing.T) {
+
+		rType, minTime, maxTime := meanstoend.DecodeInput(queryInput)
+
+		rt := meanstoend.ParseRequestType(rType)
+		t.Log(rt, minTime, maxTime)
+
+		assert.Equal(t, meanstoend.RequestType(2), rt)
+		assert.Equal(t, 1000, minTime)
+		assert.Equal(t, 10000, maxTime)
+	})
+
+	t.Run("invalid request type", func(t *testing.T) {
+
+		rType, minTime, maxTime := meanstoend.DecodeInput(invalidInput)
+
+		rt := meanstoend.ParseRequestType(rType)
+		t.Log(rt, minTime, maxTime)
+
+		assert.Equal(t, meanstoend.RequestType(0), rt)
 	})
 }
