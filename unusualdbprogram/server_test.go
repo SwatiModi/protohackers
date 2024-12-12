6d36dd80d2f29e8124e8f23c2c98a7c5221fd57b
Swati Modi <swatimodi1609@gmail.com>
2024-12-12 13:54:34 +0530

    add example test cases

--- unusualdbprogram/server_test.go
+++ unusualdbprogram/server_test.go
@@ -12,23 +12,193 @@ func TestServer(t *testing.T) {
 	// start server
 	go unusualdbprogram.StartServer()
 
-	// send version request
-	pc, err := net.Dial("udp", ":8000")
-	if err != nil {
-		t.Fatal("dial", err)
-	}
+	t.Run("version check", func(t *testing.T) {
+		// send version request
+		pc, err := net.Dial("udp", ":8000")
+		if err != nil {
+			t.Fatal("dial", err)
+		}
 
-	_, err = pc.Write([]byte("version"))
-	if err != nil {
-		t.Fatal("write", err)
-	}
+		_, err = pc.Write([]byte("version"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
 
-	// read response
-	buff := make([]byte, 1024)
-	n, err := pc.Read(buff)
-	if err != nil {
-		t.Fatal("read", err)
-	}
+		// read response
+		buff := make([]byte, 1024)
+		n, err := pc.Read(buff)
+		if err != nil {
+			t.Fatal("read", err)
+		}
 
-	assert.Equal(t, "version=0.0.1", string(buff[:n]))
+		assert.Equal(t, "version=0.0.1", string(buff[:n]))
+	})
+
+	t.Run("insert and retrieve", func(t *testing.T) {
+		// send insert request
+		pc, err := net.Dial("udp", ":8000")
+		if err != nil {
+			t.Fatal("dial", err)
+		}
+
+		_, err = pc.Write([]byte("foo=bar"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		// send retrieve request
+		_, err = pc.Write([]byte("foo"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		buff := make([]byte, 1024)
+		// read response
+		n, err := pc.Read(buff)
+		if err != nil {
+			t.Fatal("read", err)
+		}
+
+		assert.Equal(t, "foo=bar", string(buff[:n]))
+	})
+
+	t.Run("insert and retrieve 2", func(t *testing.T) {
+		// send insert request
+		pc, err := net.Dial("udp", ":8000")
+		if err != nil {
+			t.Fatal("dial", err)
+		}
+
+		_, err = pc.Write([]byte("foo=bar=baz"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		// send retrieve request
+		_, err = pc.Write([]byte("foo"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		buff := make([]byte, 1024)
+		// read response
+		n, err := pc.Read(buff)
+		if err != nil {
+			t.Fatal("read", err)
+		}
+
+		assert.Equal(t, "foo=bar=baz", string(buff[:n]))
+	})
+
+	t.Run("insert and retrieve 3", func(t *testing.T) {
+		// send insert request
+		pc, err := net.Dial("udp", ":8000")
+		if err != nil {
+			t.Fatal("dial", err)
+		}
+
+		_, err = pc.Write([]byte("foo="))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		// send retrieve request
+		_, err = pc.Write([]byte("foo"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		buff := make([]byte, 1024)
+		// read response
+		n, err := pc.Read(buff)
+		if err != nil {
+			t.Fatal("read", err)
+		}
+
+		assert.Equal(t, "foo=", string(buff[:n]))
+	})
+
+	t.Run("insert and retrieve 4", func(t *testing.T) {
+		// send insert request
+		pc, err := net.Dial("udp", ":8000")
+		if err != nil {
+			t.Fatal("dial", err)
+		}
+
+		_, err = pc.Write([]byte("foo==="))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		// send retrieve request
+		_, err = pc.Write([]byte("foo"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		buff := make([]byte, 1024)
+		// read response
+		n, err := pc.Read(buff)
+		if err != nil {
+			t.Fatal("read", err)
+		}
+
+		assert.Equal(t, "foo===", string(buff[:n]))
+	})
+
+	t.Run("insert and retrieve 5", func(t *testing.T) {
+		// send insert request
+		pc, err := net.Dial("udp", ":8000")
+		if err != nil {
+			t.Fatal("dial", err)
+		}
+
+		_, err = pc.Write([]byte("=foo"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		// send retrieve request
+		_, err = pc.Write([]byte(""))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		buff := make([]byte, 1024)
+		// read response
+		n, err := pc.Read(buff)
+		if err != nil {
+			t.Fatal("read", err)
+		}
+
+		assert.Equal(t, "=foo", string(buff[:n]))
+	})
+
+	t.Run("insert and retrieve 6", func(t *testing.T) {
+		// send insert request
+		pc, err := net.Dial("udp", ":8000")
+		if err != nil {
+			t.Fatal("dial", err)
+		}
+
+		_, err = pc.Write([]byte("message=Hello,world!"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		// send retrieve request
+		_, err = pc.Write([]byte("message"))
+		if err != nil {
+			t.Fatal("write", err)
+		}
+
+		buff := make([]byte, 1024)
+		// read response
+		n, err := pc.Read(buff)
+		if err != nil {
+			t.Fatal("read", err)
+		}
+
+		assert.Equal(t, "message=Hello,world!", string(buff[:n]))
+	})
 }
